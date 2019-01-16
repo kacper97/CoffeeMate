f4bad3ae41133b6fcbf5ee956a404986ecea6445
unknown <kacperwoloszyn4@gmail.com>
2019-01-16 11:34:58 +0000

    Adding coffee works

--- app/src/main/java/ie/cm/Home.java
+++ app/src/main/java/ie/cm/Home.java
@@ -1,5 +1,6 @@
 package ie.cm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,5 +49,10 @@ public class Home extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void add(View v)
+    {
+        startActivity(new Intent(this,Add.class));
     }
 }

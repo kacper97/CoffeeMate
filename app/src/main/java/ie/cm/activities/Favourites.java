bb3a34ae3faee87350b151efee9cb3245395dcaf
unknown <kacperwoloszyn4@gmail.com>
2019-01-30 12:25:50 +0000

    Search and filter lists

--- app/src/main/java/ie/cm/activities/Favourites.java
+++ app/src/main/java/ie/cm/activities/Favourites.java
@@ -1,11 +1,20 @@
 package ie.cm.activities;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import ie.cm.R;
+import ie.cm.fragments.CoffeeFragment;
 
-public class Favourites extends AppCompatActivity {
+public class Favourites extends Base {
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        coffeeFragment = CoffeeFragment.newInstance(); //get a new Fragment instance
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, coffeeFragment)
+                .commit(); // add it to the current activity
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/ie/cm/activities/Search.java
+++ app/src/main/java/ie/cm/activities/Search.java
@@ -1,17 +1,23 @@
-
 package ie.cm.activities;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import ie.cm.R;
+import ie.cm.fragments.CoffeeFragment;
 
-public class Search extends AppCompatActivity {
+public class Search extends Base {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.
-                search);
+        setContentView(R.layout.search);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        coffeeFragment = CoffeeFragment.newInstance(); //get a new Fragment instance
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, coffeeFragment)
+                .commit(); // add it to the current activity
+    }
+
 }

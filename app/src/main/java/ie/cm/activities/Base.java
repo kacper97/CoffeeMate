bc65a05a5430f4b52b7fdc1e96da6972b244eb86
unknown <kacperwoloszyn4@gmail.com>
2019-02-27 10:01:34 +0000

    bug fixes

--- app/src/main/java/ie/cm/activities/Base.java
+++ app/src/main/java/ie/cm/activities/Base.java
@@ -11,10 +11,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Base extends Activity {
+public class Base extends AppCompatActivity {
     public  CoffeeMateApp app;
     protected Bundle activityInfo;
     public CoffeeFragment coffeeFragment;
